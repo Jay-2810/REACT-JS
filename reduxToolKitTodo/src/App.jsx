9f5d35abb855f70b748884241a8c9cd15ce0705a
Jay-2810 <202201187@daiict.ac.in>
2025-09-10 15:18:56 +0530

    Redux toolkit

---
+++ reduxToolKitTodo/src/App.jsx
@@ -0,0 +1,15 @@
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-center mt-5'>Redux ToolKit</h1>
+      <AddTodo />
+      <Todos />
+    </>
+  )
+}
+
+export default App
