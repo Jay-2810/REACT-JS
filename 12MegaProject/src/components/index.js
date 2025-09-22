3f8b7953d19242166120cd6d33994e57bdf960cc
Jay-2810 <202201187@daiict.ac.in>
2025-09-22 14:08:29 +0530

    RTE & PostForm

--- 12MegaProject/src/components/index.js
+++ 12MegaProject/src/components/index.js
@@ -1,10 +1,11 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Logo from "./Logo";
-import Container from "./container/Container";
+import Container from "./Container/Container";
 import LogoutBtn from "./Header/LogoutBtn";
 import Button from "./Button";
 import Input from "./Input";
+import RTE from "./RTE";
 export{
     Header,
     Footer,
@@ -13,4 +14,5 @@ export{
     Container,
     Button,
     Input,
+    RTE,
 };
