f0697c54b11030babe66055cc832e86a17e9a796
Jay-2810 <202201187@daiict.ac.in>
2025-09-24 15:07:56 +0530

    debuging

--- 12MegaProject/src/components/Signup.jsx
+++ 12MegaProject/src/components/Signup.jsx
@@ -2,7 +2,7 @@ import React,{useState} from "react";
 import authService from "../appwrite/auth";
 import { Link,useNavigate } from "react-router-dom";
 import { login } from "../store/authSlice";
-import {Button,Input,Logo} from "./index";
+import {Button,Input,Logo} from "./index.js";
 import { useDispatch } from "react-redux";
 import { useForm } from "react-hook-form";
 
@@ -11,7 +11,7 @@ function Signup(){
     const navigate=useNavigate()
     const [error,setError]=useState("")
     const dispatch=useDispatch()
-    const [register,handleSubmit]=useForm()
+    const {register,handleSubmit}=useForm()
 
     const create = async (data) => {
         setError("")
@@ -50,7 +50,7 @@ function Signup(){
                     <div className="space-y-5">
                         <Input 
                         label="Full Name: "
-                        placeholder="Enter your name"
+                        placeholder="Enter your full name"
                         {...register("name",{
                             required: true,
                         })}
